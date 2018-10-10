997a0118738d8b42af8a32be54e433280afd7283
Adrian Chiavelli Hernandez (RIT Student) <arc8440@ad.rit.edu>
2018-10-10 14:43:58 -0400

    Setting resolution part 1
    Version 0.0.017

---
+++ LNS/Assets/Scripts/DefaultCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultCamera : MonoBehaviour {
+
+    public Vector2 resolution = new Vector2(1200, 600);
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution((int)resolution.x, (int)resolution.y, false);
+	}
+}
